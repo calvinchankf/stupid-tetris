e38ff8f1820d3fa97d217ea9cb1ae5ec1ec5792b
calvinchankf <chan9118kin@gmail.com>
2019-11-24 23:51:13 +0800

    Updates

---
+++ precache-manifest.cb0fb876d5b95f8d9dbbf95d56638bf0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba85a82e18ac02641ea79f70bb95c440",
+    "url": "/stupid-tetris/index.html"
+  },
+  {
+    "revision": "d56327af9d191d5db023",
+    "url": "/stupid-tetris/static/css/main.8c66ca65.chunk.css"
+  },
+  {
+    "revision": "606458b5d09e289ed425",
+    "url": "/stupid-tetris/static/js/2.7dc543d3.chunk.js"
+  },
+  {
+    "revision": "d56327af9d191d5db023",
+    "url": "/stupid-tetris/static/js/main.d689487b.chunk.js"
+  },
+  {
+    "revision": "ccdada84afdd6978fd83",
+    "url": "/stupid-tetris/static/js/runtime-main.6a9af912.js"
+  }
+]);
